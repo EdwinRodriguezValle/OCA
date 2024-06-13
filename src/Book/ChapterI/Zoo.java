826c472f9bd979036be3ad0198d52a92eef0b589
EdwinRodriguezValle <rodriguez.valle.edwin@gmail.com>
2024-06-13 16:47:55 +0200

    Import

---
+++ src/Book/ChapterI/Zoo.java
@@ -0,0 +1,15 @@
+package Book.ChapterI;
+
+
+public class Zoo {
+    /**
+     * Para compilar desde la línea de comando necesitas tienes que primero ejecutar el comando 'javac + el nombre de la clase"
+     * este genera un archivo de bytes y para imprimir o ejecutar hace java + nombre de archivo y si tiene argumentos le pasas la cantidad
+     * de argumentos.
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+    }
+}
